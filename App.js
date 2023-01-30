ad5a544257636e4f5620d2651a74fa76113faaff
corea2244 <59365194+corea2244@users.noreply.github.com>
2023-01-30 21:09:07 +0900

    Tab-Navigator(1) 사용하기

--- App.js
+++ App.js
@@ -4,20 +4,35 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ScreenA } from "./src/ScreenA";
 import { ScreenB } from "./src/ScreenB";
 import { NestedStackNavigation } from "./src/NestedStackNavigator";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { TabA } from "./src/TabA";
+import { TabB } from "./src/TabB";
 
 const Stack = createNativeStackNavigator();
+const BottomTab = createBottomTabNavigator();
+
+// export default function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="ScreenA" component={ScreenA}></Stack.Screen>
+//         <Stack.Screen name="ScreenB" component={ScreenB}></Stack.Screen>
+//         <Stack.Screen
+//           name="Nested"
+//           component={NestedStackNavigation}
+//         ></Stack.Screen>
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="ScreenA" component={ScreenA}></Stack.Screen>
-        <Stack.Screen name="ScreenB" component={ScreenB}></Stack.Screen>
-        <Stack.Screen
-          name="Nested"
-          component={NestedStackNavigation}
-        ></Stack.Screen>
-      </Stack.Navigator>
+      <BottomTab.Navigator>
+        <BottomTab.Screen name={"TabA"} component={TabA} />
+        <BottomTab.Screen name={"TabB"} component={TabB} />
+      </BottomTab.Navigator>
     </NavigationContainer>
   );
 }
